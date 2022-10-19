b75a6f0c5ef688c92eeba0a70321423487fde966
Frans Imanuel <fransimanuel99@gmail.com>
2022-10-19 17:06:23 +0700

    Initial commit

---
+++ WorkQueues/new_task/new_task.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+
+func main()  {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch,err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q,err := ch.QueueDeclare(
+		"task_queue", // name
+		true,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 5)
+	defer cancel()
+
+	body := bodyForm(os.Args)
+	err = ch.PublishWithContext(ctx,
+		"", //exchange
+		q.Name, //routing key
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType: "text/plain",
+			Body: []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf("[x] Sent %s", body)
+}
+
+func bodyForm(args []string) string{
+	var s string
+	if len(args) < 2 || os.Args[1] == ""{
+
+	}else{
+		s = strings.Join(args[1:],"")
+	}
+
+	return s
+}
